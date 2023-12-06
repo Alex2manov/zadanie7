5af6529ddf6959a7687d8d64fd42f2fdfb7c0966
Alex2manov <144853661+Alex2manov@users.noreply.github.com>
2023-12-06 12:26:18 +0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.galery').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    })
+});
