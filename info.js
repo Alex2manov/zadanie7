cb508ea1f3756f04942e42985b03386598d83bd7
Alex2manov <144853661+Alex2manov@users.noreply.github.com>
2023-12-14 08:19:09 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){  //Код, включенный в $( document ).ready(), будет выполняться только тогда, когда объектная модель документа (DOM) страницы будет готова к выполнению кода JavaScript. Код, включенный в $( window ).on( "load", function() { ... }), запустится, как только будет готова вся страница (изображения или iframe), а не только DOM.
     $('.galery').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
